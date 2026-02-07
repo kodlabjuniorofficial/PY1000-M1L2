ad4b99fd90b4e2f04eaf992d8f5b556bcd0d78e6
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 14:01:42 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+#t.    # kaplumbağanın rengi
+#t.       # hızını ayarlama
+#t.            # kalemi kaldır (çizmeden yürü)
+#t.forward(100)
+#t. # kalemi indir (çizmeye devam et)
+#t.forward(100)
+
+
+
+# kaplumbağanın rengini değiştir
+# hızını ayarlama
+# kalemi kaldır (çizmeden yürü)
+# kalemi indir (çizmeye devam et)
