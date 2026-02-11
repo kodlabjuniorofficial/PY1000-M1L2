90d4e2ebaea80b56c05ada4e5980cdc1bb9b8b6f
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-11 13:44:58 +0300

    Create 5.py with variable operations and prints
    Add initial implementation for basic variable operations and printing.

---
+++ 5.py
@@ -0,0 +1,14 @@
+# sayısal iki değişken oluştur, istediğin değeri verebilirsin
+
+# adını ve soyadını tutan iki değişken oluştur
+
+# sayısal değişkenler ile şu işlemleri yap: toplama, çıkarma.çarpma,bölme
+print()
+print()
+print()
+print()
+# adını 4 defa yan yana yazdır! 
+print()
+
+# ad ve soyadını aralarında boşluk olacak şekilde yazdır
+print()
