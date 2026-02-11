07a915d647477960cf9dee2329875f443b7fa7e8
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-11 13:44:24 +0300

    Add basic arithmetic and string operations in 4.py

---
+++ 4.py
@@ -0,0 +1,26 @@
+# Sayısal + Sayısal
+a = 5
+b = 3
+print(a + b)   # 8
+print(a - b) # 2
+
+# Sayısal * Sayısal
+a = 10
+b = 5
+print(a*b) # 50
+print(a/b) # 2
+print(8*9) # 72
+print(10/2) # 5
+
+# Metinsel * Sayısal
+ad = "Kaplumbağa"
+kere = 4
+print(ad*kere) # KaplumbağaKaplumbağaKaplumbağa
+
+# Metinsel + Metinsel
+isim = "Elif"
+soyisim = "Yıldız"
+print(isim+soyisim)
+
+# Metinsel/Metinsel yapılamaz
+# Metinsel+Sayısal yapılamaz (str yardımı gerek)
