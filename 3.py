082601bf19ec008dbf78dae7c05c18ed694f075c
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-11 13:43:46 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,6 @@
+# Bu noktada kütüphaneyi içeri aktarın ve kaplumbağayı canlandırın! 
+
+# Görev:
+# Kaplumbağanın kaç adım atacağını belirleyen bir değişken oluştur
+# Kaplumbağanın rengini belirleyen bir değişken oluştur
+# Değişkenleri kullanarak kendi şeklini çiz
